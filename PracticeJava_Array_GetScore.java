be1f3746b372095070708686fb85d8e4c7659223
leehyunyong89 <74758862+leehyunyong89@users.noreply.github.com>
2021-05-17 00:19:28 +0900

    [자바코딩연습]_[다차원배열]_[평균점수구하기]_[v1.0]_20210516
    철수의 과목별 평균, 학기별 평균점수 구하기.
    일단 문제만 써서 업로드 함.

---
+++ PracticeJava_Array_GetScore.java
@@ -0,0 +1,12 @@
+package sec01.exam03;
+
+public class PracticeJava_Array_GetScore {
+
+	public static void main(String[] args) {
+		//(문제) 철수의 성적은 아래와 같다.(성적은 노션에 저장된 이미지 참고) 
+		//성적을 메모리에 저장하고 과목별 평균과 학기별 평균을 구하시오.
+
+
+	}
+
+}
